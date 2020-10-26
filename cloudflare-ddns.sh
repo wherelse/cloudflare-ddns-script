f7b142a7bc7c50337aa8906f672b6d02a061bc98
dynamicfire <plus@plusnow.me>
2020-10-26 10:07:04 +0800

    Added backslash and remove spaces
    Added missing backslash and remove redundant spaces

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -76,8 +76,8 @@ else
         -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
     record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" \
         -H "X-Auth-Email: $auth_email" \
-        -H "X-Auth-Key: $auth_key" 
-        -H "Content-Type: application/json"  | grep -Po '(?<="id": ")[^"]*')
+        -H "X-Auth-Key: $auth_key" \
+        -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*')
     echo "$zone_identifier" > $id_file
     echo "$record_identifier" >> $id_file
 fi
@@ -91,7 +91,7 @@ update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identi
 
 
 #反馈更新情况 gave the feedback about the update statues
-if [[ "$(echo $update | grep "\"success\": true")" != "" ]]; then
+if [[ "$(echo $update | grep "\"success\":true")" != "" ]]; then
     message="IP changed to: $ip"
     echo "$ip" > $ip_file
     log "$message"
